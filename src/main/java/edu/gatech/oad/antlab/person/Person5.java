f13f2438ca351573da34d2686e35f2fe71472217
Steven Chen <steven@lawn-128-61-22-228.lawn.gatech.edu>
2019-09-13 18:51:03 -0400

    Filled in Person5 method

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Steven
  *  @version 1.1
  */
 public class Person5 {
@@ -31,7 +31,13 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+	  if (input.length() == 1) {
+		  return input;
+	  } else if (input.length()== 2) {
+		  return input.charAt(1) + input.charAt(0);
+	  } else {
+		  return  input.substring(2, input.length()) + input.substring(0, 2);
+	  }
 	}
 	
 	/**
